a19b85c435e0893fb1645a928f6ca2ccc2357078
joonghyun <joongh@ewhain.net>
2021-07-03 00:49:24 +0900

    4.3 Adding Genres

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ class App extends React.Component {
    render() {
       const { isLoading, movies } = this.state;
       return (
-         <section class="container">
+         <section className="container">
             {isLoading ? (
-               <div class="loader">
-                  <span class="loader__text">Loading...</span>
+               <div className="loader">
+                  <span className="loader__text">Loading...</span>
                </div>
             ) : (
-               <div class="movies">
+               <div className="movies">
                   {movies.map((movie) => {
                      return (
                         <Movie
@@ -40,6 +40,7 @@ class App extends React.Component {
                            title={movie.title}
                            summary={movie.summary}
                            poster={movie.medium_cover_image}
+                           genres={movie.genres}
                         />
                      );
                   })}
